b078063f465fc714db09504b8e7bdb199646f10a
Aaron Tribou <aaron.tribou@gmail.com>
2017-05-02 10:42:59 -0500

    Add blacklist option

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 // @flow
 import WebpackMd5Hash from 'webpack-md5-hash'
 
-const isVendor = ({ userRequest }) => (
+type Blacklist = string | RegExp
+
+type Options = {
+  name: string,
+  blacklist?: Blacklist,
+}
+
+const match = (userRequest, matcher?: Blacklist) => {
+  if (!matcher) return true
+  if (matcher instanceof RegExp) return !userRequest.match(matcher)
+  return userRequest.indexOf(matcher) === -1
+}
+
+const isVendor = ({ blacklist }: Options) => ({ userRequest }) => (
   userRequest &&
   userRequest.indexOf('node_modules') >= 0 &&
-  userRequest.match(/\.js$/)
+  userRequest.match(/\.js$/) &&
+  match(userRequest, blacklist)
 )
 
 /**
  * Returns a webpack block that splits vendor javascript bundle.
  */
-const splitVendor = (name: string = 'vendor'): any => ({ webpack }) => ({
+const splitVendor = (name: string | Options = 'vendor'): any => ({ webpack }) => ({
   output: {
     filename: '[name].[chunkhash].js',
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
-      minChunks: isVendor,
-      name,
+      minChunks: typeof name === 'object' ? isVendor(name) : isVendor({ name }),
+      name: typeof name === 'object' ? name.name : name,
     }),
     new WebpackMd5Hash(),
   ],
