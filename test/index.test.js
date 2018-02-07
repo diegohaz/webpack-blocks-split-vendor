831bbdaf6c5f41d0702b5f29f0bc1fb283ba1b13
Diego Haz <hazdiego@gmail.com>
2018-02-07 13:27:47 -0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,25 @@
+import { createConfig } from 'webpack-blocks'
 import splitVendor from '../src'
 
 describe('splitVendor', () => {
   it('returns webpack block with passed string name', () => {
-    const block = splitVendor('foo')()()
-    expect(block.plugins[0].chunkNames[0]).toBe('foo')
+    const config = createConfig([
+      splitVendor('foo'),
+    ])
+    expect(config.plugins[0].chunkNames[0]).toBe('foo')
   })
 
   it('returns webpack block with passed object name', () => {
-    const block = splitVendor({ name: 'foo' })()()
-    expect(block.plugins[0].chunkNames[0]).toBe('foo')
+    const config = createConfig([
+      splitVendor({ name: 'foo' }),
+    ])
+    expect(config.plugins[0].chunkNames[0]).toBe('foo')
   })
 
   it('returns webpack block with default name', () => {
-    const block = splitVendor()()()
-    expect(block.plugins[0].chunkNames[0]).toBe('vendor')
+    const config = createConfig([
+      splitVendor(),
+    ])
+    expect(config.plugins[0].chunkNames[0]).toBe('vendor')
   })
 })
